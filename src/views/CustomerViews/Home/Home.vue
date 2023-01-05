c68c4744d5a44bf6b375eb4f04a634c0660e9a40
pires lufungula <pires00243@gmail.com>
2023-01-04 23:55:24 -0500

    Add extra text

--- src/views/CustomerViews/Home/Home.vue
+++ src/views/CustomerViews/Home/Home.vue
@@ -7,12 +7,20 @@
       <!--Our values-->
       <Our-values></Our-values>
     </div>
+      <div class="bg-secondary text-center p-4 mb-4 mt-4">
+        <div class="container">
+          <p>En parfaite conformité avec les lois des pays concernés, Orichid Campus propose des services d'assistance. En plus des vidéos tutorielles que nous fournissons, nous proposons également un service d'accompagnement dont le but est la réalisation de votre projet académique plutôt que de gagner de l'argent.</p>
+          <ButtonComponent></ButtonComponent>
+        </div>
+           </div>
+    
    </parent-component> 
 </template>
 
 <script>
 import Carousel from '../../../components/Customer/Home/Carousel.vue';
-import OurValues from '../../../components/Customer/Home/OurValues.vue';
+import OurValues from '../../../components/Customer/Home/OurValues.vue'; 
+import ButtonComponent from '../../../components/shared/Menu/ButtonComponent.vue';
 import ParentComponent from '../../../components/shared/ParentComponent.vue'
  
 export default{
@@ -30,7 +38,7 @@ export default{
    // this.nameApp = import.meta.env.APP_TITLE
   },   
   
-  components: { ParentComponent, Carousel, OurValues },
+  components: { ParentComponent, Carousel, OurValues, ButtonComponent },
  
     name : 'Home'
 }
