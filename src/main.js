a064867f73cdb12c0014924ff9bba3be811a5b4e
pires lufungula <pires00243@gmail.com>
2023-01-06 16:21:28 -0500

    import icon

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
 //import './style.css'
 import App from './App.vue' 
 import "bootstrap/dist/css/bootstrap.css"
@@ -6,7 +14,10 @@ import "bootstrap/dist/css/bootstrap.css"
 // Import our custom CSS
 import './styles.scss'
 
- 
+/* import specific icons */  
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faInstagramSquare, faMailchimp, faWhatsapp, faWhatsappSquare } from '@fortawesome/free-brands-svg-icons'
 
  
-createApp(App).mount('#app')
+library.add(faWhatsappSquare,faEnvelope,faInstagramSquare,faMailchimp) 
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
