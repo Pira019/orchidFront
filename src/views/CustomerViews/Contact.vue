da0e90cdfb18f4a4f6d3892d1e2c40085a10b420
pires lufungula <pires00243@gmail.com>
2023-01-14 03:46:39 -0500

    clean code

---
+++ src/views/CustomerViews/Contact.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">Contact</h1>
+
+        <p>{{ id }}</p>
+
+        <RouterView></RouterView>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        id:this.id
+    }
+  },
+   
+    name: "Contact"
+}
+</script>
--- src/views/CustomerViews/Faq.vue
+++ src/views/CustomerViews/Faq.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="container mt-5 mb-5">
+    <div class="container">
         <h1 class="text-center text-success">Faqs</h1> 
         <faq-component :faqs="contentFaqs" class="mt-5"></faq-component>
+
+        <div class="text-center bg-info mt-5 mb-3 pt-3 pb-3">
+          <h3 class="text-success">Vous avez encore des questions ?</h3>
+          <p>Vous pouvez nous contacter <router-link class="text-reset" :to="{name:'contact'}">ici</router-link> </p>
+        </div>
     </div>    
 </template>
 
