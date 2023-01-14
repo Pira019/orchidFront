f1a52562c06b3d0c6b5b773df8d3154b779ff982
pires lufungula <pires00243@gmail.com>
2023-01-14 03:46:54 -0500

    clean code

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import ParentComponent from '@/components/shared/ParentComponent.vue';
 </script>
 <template>
 <parent-component>
-    <router-view></router-view>
+    <router-view class="mt-5 mb-5"></router-view>
 </parent-component>
 </template>
  
