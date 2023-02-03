d7935befe33b67a2ab70b4ef4c15ecf3c8115159
pires lufungula <pires00243@gmail.com>
2023-02-02 20:33:13 -0500

    captcha to review

---
+++ src/components/shared/RecaptchaComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <VueRecaptcha
+      :sitekey="siteKey"
+      :load-recaptcha-script="true"
+      @verify="handleSuccess"
+      @error="handleError"
+    ></VueRecaptcha>
+  </template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { VueRecaptcha} from 'vue-recaptcha'
+export default{
+    components : {VueRecaptcha},
+    setup(){
+        const siteKey = computed(() => {
+            return "6Lcm2z0kAAAAAJL-i0M_-FufN5qXLHlcru4TjYlv"
+        });
+
+    const handleError = () => {
+      // Do some validation
+    };
+
+    const handleSuccess = (response) => {
+     // Do some validation
+    };
+
+    return {
+      handleSuccess,
+      handleError,
+      siteKey,
+    };
+    },
+    name : 'RecaptchaComponent'
+
+}
+</script>
