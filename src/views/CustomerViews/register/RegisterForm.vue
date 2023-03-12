ba9bb6960f10736b24e201285d577cb2895b0596
pires lufungula <pires00243@gmail.com>
2023-03-12 16:03:15 -0400

    Update package

--- src/views/CustomerViews/register/RegisterForm.vue
+++ src/views/CustomerViews/register/RegisterForm.vue
@@ -2,16 +2,16 @@
     <form novalidate v-on:submit.prevent="onsubmit">
     <div>  
         <div class="row mb-3 no-gutter">
-            <div class="col">  
-                <input type="text" class="form-control rounded-0" autocomplete="family-name" name="firstName" :class="[v$.user.firstName.$error ? 'is-invalid' : '']" v-model.trim="state.user.firstName" placeholder="Prénom*" title="Prénom*" required>
-                <div class="invalid-feedback" v-if="v$.user.firstName.$error">
-                    <span v-for="(error,index) of v$.user.firstName.$errors" :key="index" >
+            <div class="col">   
+                <input type="text" class="form-control rounded-0" autocomplete="family-name" name="first_name" :class="[v$.user.first_name.$error ? 'is-invalid' : '']" v-model.trim="state.user.first_name" placeholder="Prénom*" title="Prénom*" required>
+                <div class="invalid-feedback" v-if="v$.user.first_name.$error">
+                    <span v-for="(error,index) of v$.user.first_name.$errors" :key="index" >
                     {{ error.$message }}
                     </span>
                  </div>                
             </div>
             <div class="col">
-                <input type="text" class="form-control rounded-0" name="name" autocomplete="family-name" :class="[v$.user.name.$error ? 'is-invalid' : '']" v-model="state.user.name" placeholder="Nom*" title="Nom*"  required>
+                <input type="text" class="form-control rounded-0" name="name" autocomplete="family-name" :class="[v$.user.name.$error ? 'is-invalid' : '']" v-model.trim="state.user.name" placeholder="Nom*" title="Nom*"  required>
                 <div class="invalid-feedback" v-if="v$.user.name.$error">
                     <span v-for="(error,index) of v$.user.name.$errors" :key="index" >
                     {{ error.$message }}
@@ -33,16 +33,16 @@
                  </div> 
             </div>
             <div class="col">
-                <input type="text" class="form-control rounded-0" placeholder="Date de naissance" onfocus="(this.type='date')" onblur="this.type='text'" title="Date de naissance">
+                <input type="date" class="form-control rounded-0" placeholder="Date de naissance" onfocus="(this.type='date')" onblur="this.type='text'" v-model="state.user.birth_date"  title="Date de naissance">
             </div>
         </div>     
         
         <div class="row mb-3">
             <div class="col">
-                <select class="form-select form-control rounded-0" name="country" autocomplete="coutry"  :class="[v$.user.residenceContry.$error ? 'is-invalid' : '']"  v-model="state.user.residenceContry" title="Pays de résidence *" required>
+                <select class="form-select form-control rounded-0" name="country" autocomplete="coutry"  :class="[v$.user.residenceContry.$error ? 'is-invalid' : '']"  v-model="state.user.residenceContry" title="Pays de résidence *" >
                     <option selected value="">Pays de résidence ...</option>
-                    <option value="M">Masculin</option>
-                    <option value="F">Féminin</option> 
+                    <option value="">Masculin</option>
+                    <option value="">Féminin</option> 
                 </select> 
                 <div class="invalid-feedback" v-if="v$.user.residenceContry.$error">
                     <span v-for="(error,index) of v$.user.residenceContry.$errors" :key="index" >
@@ -51,10 +51,10 @@
                  </div> 
             </div>
             <div class="col">
-                <select class="form-select form-control rounded-0"   name="citezenship" autocomplete="coutry" :class="[v$.user.citezenship.$error ? 'is-invalid' : '']" title="Pays de citoyenneté *" v-model="state.user.citezenship" required>
+                <select class="form-select form-control rounded-0"   name="citezenship" autocomplete="coutry" :class="[v$.user.citezenship.$error ? 'is-invalid' : '']" title="Pays de citoyenneté *" v-model="state.user.citezenship" >
                     <option selected value="">Pays de la citoyenneté ...</option>
-                    <option value="M">Masculin</option>
-                    <option value="F">Féminin</option> 
+                    <option value="">Masculin</option>
+                    <option value="">Féminin</option> 
                 </select> 
                 <div class="invalid-feedback" v-if="v$.user.citezenship.$error">
                     <span v-for="(error,index) of v$.user.citezenship.$errors" :key="index" >
@@ -66,7 +66,7 @@
 
         <div class="row mb-3">
             <div class="col">
-                <input type="tel" :class="[v$.user.phone.$error ? 'is-invalid' : '']" name="tel" autocomplete="tel" title="Numéro téléphone" class="form-control rounded-0"  v-model="state.user.phone" placeholder="Téléphone">
+                <input type="tel" :class="[v$.user.phone.$error ? 'is-invalid' : '']" name="tel" autocomplete="tel" title="Numéro téléphone" class="form-control rounded-0"  v-model.trim="state.user.phone" placeholder="Téléphone">
                  <div class="invalid-feedback" v-if="v$.user.phone.$error">
                     <span v-for="(error,index) of v$.user.phone.$errors" :key="index" >
                     {{ error.$message }}
@@ -77,7 +77,7 @@
 
         <div class="row mb-3">
             <div class="col">
-                <input type="email" class="form-control rounded-0"  autocomplete="email" name="email" placeholder="Email*"  title="Email*" :class="[v$.user.email.$error ? 'is-invalid' : '']" v-model="state.user.email" required>
+                <input type="email" class="form-control rounded-0"  autocomplete="email" name="email" placeholder="Email*"  title="Email*" :class="[v$.user.email.$error ? 'is-invalid' : '']" v-model.lazy="state.user.email" required>
                 <div class="invalid-feedback" v-if="v$.user.email.$error">
                     <span v-for="(error,index) of v$.user.email.$errors" :key="index" >
                     {{ error.$message }}
@@ -86,19 +86,19 @@
             </div>            
         </div>
 
-        <div class="row mb-3">
-            <div class="col input-group">
-                <input type="password" class="form-control rounded-0" :class="[v$.user.password.password.$error ? 'is-invalid' : '']" v-model="state.user.password.password" placeholder="Mot de passe *" title="Mot de passe *" required> 
-                <div class="invalid-feedback" v-if="v$.user.password.password.$error">
-                    <span v-for="(error,index) of v$.user.password.password.$errors" :key="index" >
+        <div class="row mb-3 input-group">
+            <div class="col ">
+                <input type="password" class="form-control rounded-0" :class="[v$.user.password.$error ? 'is-invalid' : '']" v-model="state.user.password" placeholder="Mot de passe *" title="Mot de passe *" required> 
+                <div class="invalid-feedback" v-if="v$.user.password.$error">
+                    <span v-for="(error,index) of v$.user.password.$errors" :key="index" >
                     {{ error.$message}} 
                     </span>
                  </div> 
             </div>
             <div class="col">
-                <input type="password" class="form-control rounded-0" required :class="[v$.user.password.confirm.$error ? 'is-invalid' : '']" v-model="state.user.password.confirm" placeholder="Confirmer le mot de passe *" title="Confirmer le mot de passe *">
-                <div class="invalid-feedback" v-if="v$.user.password.confirm.$error">
-                    <span v-for="(error,index) of v$.user.password.confirm.$errors" :key="index" >
+                <input type="password" class="form-control rounded-0" required :class="[v$.user.password_confirmation.$error ? 'is-invalid' : '']" v-model="state.user.password_confirmation" placeholder="Confirmer le mot de passe *" title="Confirmer le mot de passe *">
+                <div class="invalid-feedback" v-if="v$.user.password_confirmation.$error">
+                    <span v-for="(error,index) of v$.user.password_confirmation.$errors" :key="index" >
                     {{ error.$message }}
                     </span>
                  </div> 
@@ -106,7 +106,7 @@
         </div>
         <div class="row">
             <div class="col">
-                <SubmitBtnComponent @click="submitForm" class="btn btn-success">Créer mon compte</SubmitBtnComponent>
+                <SubmitBtnComponent :loading="loading" @click="submitForm" class="btn btn-success">Créer mon compte</SubmitBtnComponent> 
             </div>
         </div>
     
@@ -118,7 +118,7 @@ import SubmitBtnComponent from '@/components/shared/SubmitBtnComponent.vue';
 import {required,email,sameAs} from '@vuelidate/validators' 
 import useVuelidate from '@vuelidate/core';
 import { computed, reactive } from 'vue';
-import { useStore } from 'vuex'; 
+import { mapActions, mapGetters, mapState, useStore } from 'vuex'; 
 
 export default{
     setup(){
@@ -129,36 +129,51 @@ export default{
      return {         
            user:{ 
             name : {required},
-            firstName : {required},
+            first_name : {required},
             sex : {required},
-            residenceContry : {required},
+            birth_date:{},
+            residenceContry : {},
             sex : {required},
-            citezenship : {required}, 
+            citezenship : {}, 
             email : {required,email},
-            phone : {},
-            password : {
-                password:{required},
-                confirm : {required,sameAs : sameAs(state.user.password.password)}
+            phone : {}, 
+            password:{required},
+            password_confirmation : {required,sameAs : sameAs(state.user.password)}
             }
             
          }
      
-    }})
-    const v$ = useVuelidate(rules,state)
-    console.log(rules)
+    });
+
+    const v$ = useVuelidate(rules,state) 
     return{
         state,
         v$
     }
 }, 
- 
-    methods:{
-        submitForm(){  
-            this.v$.$validate()            
-       //    console.log(this.v$.user.password.confirm.$errors[0].$message)     
-           //  console.log(this.v$.user.firstName.$errors[0].$message)
+    data(){
+        return {
+            loading : false
         }
     },
+ 
+    computed:{  
+ 
+        
+    },
+    methods:{
+         submitForm(){  
+            this.loading =true;
+            this.v$.$validate()
+            if(!this.v$.$error){                
+                this.$store.dispatch('user/saveUser', this.state.user);
+                this.loading = false;
+                return true;
+              //console.log(this.state.user)
+            } 
+        }
+        
+        },
     name: "RegisterForm",
     components: { SubmitBtnComponent }
 }
