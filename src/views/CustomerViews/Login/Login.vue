592a37ac06f26fc858b4045134c9bb16721632de
pires lufungula <pires00243@gmail.com>
2023-05-08 23:32:33 -0400

    test and show the de modal if user is created else hide succeed modal

--- src/views/CustomerViews/Login/Login.vue
+++ src/views/CustomerViews/Login/Login.vue
@@ -5,19 +5,36 @@
             <div class="col-md-6 p-5 d-sm-none d-none d-md-block" :style="imgLogin" style="background-position: center;   background-size: cover">                
             </div>
         </div>   
+        <register-success-modal-component  v-if="showSucceedModal"></register-success-modal-component> 
     </div>
 </template>
 <script>
 import FormLogin from './FormLogin.vue';
-import imgLogin from '@/assets/img/graduation.jpg';
+import imgLogin from '@/assets/img/graduation.jpg';  
+import { ModalTypeEnum } from '@/enums';
+import RegisterSuccessModalComponent from '@/components/modal/RegisterSuccessModalComponent.vue';
 
 export default  {
+  created()
+  { 
+    //test and show the de modal if user is created else hide succeed modal     
+    if(this.$store.getters.getState){
+      this.showSucceedModal=true      
+      this.$store.commit('setIsSucceed',false) 
+      console.log(this.showSucceedModal)
+    }else{
+     this.showSucceedModal=false;
+     console.log(this.showSucceedModal)
+    }   
+  },
   data () {
     return { 
-      imgLogin: { backgroundImage: `url('${imgLogin}')`} 
+      imgLogin: { backgroundImage: `url('${imgLogin}')`},
+      showSucceedModal : false,
+      typeModal : ModalTypeEnum.REGISTER
   };
   },
     name: "Login",
-    components: { FormLogin }
+    components: { FormLogin,RegisterSuccessModalComponent }
 }
 </script>
