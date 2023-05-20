3d163111d8d88e886f7182f3ae02f5cdc6fc2729
pires lufungula <pires00243@gmail.com>
2023-05-19 22:05:01 -0400

    add 404 error

--- src/Services/ErrorService.js
+++ src/Services/ErrorService.js
@@ -5,14 +5,15 @@ export default class ErrorService {
     static handleErrorHttp(codeStatus, message = '') {
         var errorStatus = {
             401: [[validationMessage.error.credentials_do_not_match]],
-            422: message
+            422: message,
+            404: message
         }
 
         var error = errorStatus[codeStatus];
 
         if (error) {
             return {errorMessage :error }
-        }
+        } 
     }
 
 }
