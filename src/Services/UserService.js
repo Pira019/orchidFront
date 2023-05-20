f61478c37c0807af6a1e6c3a76d0a9edcc94c5dd
pires lufungula <pires00243@gmail.com>
2023-05-19 22:05:21 -0400

    Optimise UserService

---
+++ src/Services/UserService.js
@@ -0,0 +1,19 @@
+
+import axios, * as others from 'axios';
+export default class UserService{
+   static baseApiUrl = import.meta.env.VITE_APP_API_URL;
+    /**
+   * A class for interacting with the user service API.
+   *
+   * @param {string} baseApiUrl - The base URL of back end API.
+   */
+
+    constructor() {}
+    
+   
+    static async forgotPassword(email){
+        const endPoind =this.baseApiUrl + 'forgot-password'
+        return axios.post(endPoind,email);  
+    }
+
+}
