590c8c66351b981d09bd1fa265c9ff64891ac369
pires lufungula <pires00243@gmail.com>
2023-05-22 14:42:13 -0400

    success allert

---
+++ src/components/shared/Alert/SuccessAlert.vue
@@ -0,0 +1,22 @@
+<template >
+       <div class="alert alert-success" role="alert" v-if="show"> 
+         <h4 class="alert-heading">Réinitialisation du mot de passe</h4>
+         <p>{{ response }}</p>
+        </div>
+</template>
+
+<script>
+export default {
+    props : {         
+        show: {
+            default : false,
+            type : Boolean
+        },
+        response : {
+            default : '',
+            required : true
+        }
+    },
+    name : 'SuccessAlert'
+}
+</script>
