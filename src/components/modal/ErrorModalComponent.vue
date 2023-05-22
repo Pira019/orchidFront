e4e8af1344b3e519c0cd761cf94020b01f69eab5
pires lufungula <pires00243@gmail.com>
2023-05-22 14:42:40 -0400

    unErrUnexpected error modal

---
+++ src/components/modal/ErrorModalComponent.vue
@@ -0,0 +1,44 @@
+<template> <!-- Button trigger modal --> 
+    <!-- Modal -->
+    <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModal" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header m-3">            
+            <font-awesome-icon icon="fa-solid fa-triangle-exclamation" class="modal-title w-100 text-center text-danger" size="3x" beat></font-awesome-icon>  
+          </div>
+          <div class="modal-body text-center border-0">
+            <h5>Erreur inattendue</h5> 
+            <p>Une erreur inattendue s'est produite. Veuillez réessayer ultérieurement.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button> 
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import { ModalTypeEnum } from '@/enums';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+export default {
+  created(){
+    //show the modal
+    $(document).ready(function(){
+		$("#errorModal").modal('show');
+	});
+  },
+  methods: { 
+  },
+  data () {
+    return {
+      modalName : ModalTypeEnum.REGISTER
+    }
+  },
+    name: "ErrorModalComponent",
+    components: { FontAwesomeIcon }
+}
+ 
+</script>
+ 
