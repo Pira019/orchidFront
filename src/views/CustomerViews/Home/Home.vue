2f00eb62fa8810107c816ecf10d2aff2634b17a3
pires lufungula <pires00243@gmail.com>
2023-06-06 13:19:01 -0400

    FIXE home waring

--- src/views/CustomerViews/Home/Home.vue
+++ src/views/CustomerViews/Home/Home.vue
@@ -1,35 +1,45 @@
-<template>  
+<template>
+  <div id="home">
     <Carousel></Carousel>
-    <div class="container mt-5">
+    <div class="container">
       <h2 class="text-center">Nos valeurs</h2>
       <!--Our values-->
       <Our-values></Our-values>
     </div>
-      <div class="bg-info text-center p-4 mb-4 mt-5">
-        <div class="container">
-          <p class="text-success">En parfaite conformité avec les lois des pays concernés, Orchid Campus propose des services d'assistance numérique. En plus des vidéos tutorielles que nous fournissons, nous proposons également un service d'accompagnement dont le but est la réalisation de votre projet académique plutôt que de gagner de l'argent.</p>
-          <ButtonComponent :variantStyle="btnStyle">
-         Tutoriel pour étudier à l'étranger
-          </ButtonComponent>
-        </div>
-           </div>    
- 
+    <div class="bg-info text-center p-4 mb-4 mt-5">
+      <div class="container">
+        <p class="text-success">En parfaite conformité avec les lois des pays concernés, Orchid Campus propose des
+          services d'assistance numérique. En plus des vidéos tutorielles que nous fournissons, nous proposons également
+          un service d'accompagnement dont le but est la réalisation de votre projet académique plutôt que de gagner de
+          l'argent.</p>
+        <ButtonComponent :variantStyle="btnStyle">
+          Tutoriel pour étudier à l'étranger
+        </ButtonComponent>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Carousel from '../../../components/Customer/Home/Carousel.vue';
-import OurValues from '../../../components/Customer/Home/OurValues.vue'; 
-import ButtonComponent from '../../../components/shared/Menu/ButtonComponent.vue'; 
- 
-export default{
-  data () {
+import OurValues from '../../../components/Customer/Home/OurValues.vue';
+import ButtonComponent from '../../../components/shared/Menu/ButtonComponent.vue';
+
+export default {
+  data() {
     return {
-      btnStyle:'btn-success text-white'
+      btnStyle: 'btn-success text-white'
     }
-    
-  }, 
+
+  },
   components: { Carousel, OurValues, ButtonComponent },
- 
-    name : 'Home'
+
+  name: 'Home'
 }
 </script>
+
+<style scoped>
+#home{
+ margin-top: 0% !important;
+}
+</style>
