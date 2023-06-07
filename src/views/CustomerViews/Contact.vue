e3e0e844f14909d65d239bbae3eed91f620161c6
pires lufungula <pires00243@gmail.com>
2023-06-06 23:13:38 -0400

    change text

--- src/views/CustomerViews/Contact.vue
+++ src/views/CustomerViews/Contact.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container">
         <h1 class="text-center text-success">Contact</h1>
-        <div class="mt-5 text-center">
-          <p>Avez-vous des questions ou avez-vous besoin d'informations supplémentaires? <br>
-            <strong>S'il vous plaît n'hésitez pas à nous contacter.</strong> </p>
+        <div class="text-center">
+          <h2 class="h6 lead">Nous sommes ravis de pouvoir vous aider et répondre à toutes vos questions.<br>
+            <strong>S'il vous plaît n'hésitez pas à nous contacter.</strong> </h2>
         </div>
         <contact-form-component></contact-form-component>
     </div>
