aa18180165ab7f10b7f53b376ebb06f35f28c984
pires lufungula <pires00243@gmail.com>
2023-06-08 19:08:48 -0400

    Card for tuto list

---
+++ src/views/ManagerViews/Tutorials/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container-fluid">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Drapeau pays">
+            <div class="card-body">
+                <h5 class="card-title">Nom pays</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Nombre d'étudiants</li>
+                <li class="list-group-item">Date de publication :</li>
+                <li class="list-group-item">Date Création :</li>
+                <li class="list-group-item">Dérniere modification :</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ManagerTutoHome"
+}
+</script>
