7146e69f83b5f4e4f651b54ab39376d6a08c4adf
pires lufungula <pires00243@gmail.com>
2023-06-18 02:27:15 -0400

    Add country steps form

--- src/views/ManagerViews/Tutorials/AddTutoriel/CountrySteps.vue
+++ src/views/ManagerViews/Tutorials/AddTutoriel/CountrySteps.vue
@@ -1,7 +1,24 @@
 <template>
     <div>
         <form>
-            <h1>ETDGGVDVG</h1>
+            <div class="row">
+                <div class="form-group col-sm-4">
+                    <label for="Titre">Titre *</label>
+                    <input type="text" id="Titre" class="form-control">
+                </div>
+                <div class="form-group col-sm-4">
+                    <label for="stepNum">Numéro étape *</label>                     
+                    <input type="number" min="1" class="form-control" id="stepNum" value="1">
+                </div>
+                <div class="form-group col-sm-4">
+                    <label for="Description">Description</label>   
+                    <textarea class="form-control" id="Description" rows="5"></textarea>
+                </div>
+                <div class="form-group mt-3">
+                  <button class="btn btn-primary">Ajouter une étape</button>
+                  <button class="btn btn-success mx-1">Enregister</button>
+                </div>                           
+            </div>
         </form>
     </div>
 </template>
