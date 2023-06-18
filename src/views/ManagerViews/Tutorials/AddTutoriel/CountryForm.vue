4b2a307d0b4351d7ddb1b649d5f41482aea8be58
pires lufungula <pires00243@gmail.com>
2023-06-18 02:26:45 -0400

    Fix menu add tuto

--- src/views/ManagerViews/Tutorials/AddTutoriel/CountryForm.vue
+++ src/views/ManagerViews/Tutorials/AddTutoriel/CountryForm.vue
@@ -4,13 +4,13 @@
             <div class="form-group row">
                 <label for="conutry" class="col-sm-2 col-form-label fw-bold">Selectionner le pays</label>
                 <div class="col-sm-4 mb-3">
-                  <select class="form-control" id="conutry">
+                  <select class="form-select" id="conutry">
                     <option value="">TEST 1</option>
-                    <option value="">TEST 1</option>
+                    <option value="">TEST 145</option>
                   </select>
                 </div>
                 <div  class="col-sm-4">
-                    <SubmitBtnComponent class="btn btn-success">Enregister</SubmitBtnComponent>
+                    <SubmitBtnComponent class="btn btn-success">Continuer</SubmitBtnComponent>
                 </div>
               
             </div>
