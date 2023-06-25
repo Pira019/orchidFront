de9be6777b71f1f479e1bf95c3ceb5554447e00f
pires lufungula <pires00243@gmail.com>
2023-06-25 01:43:35 -0400

    Clean code

--- src/views/ManagerViews/Tutorials/AddTutoriel/AddTutorial.vue
+++ src/views/ManagerViews/Tutorials/AddTutoriel/AddTutorial.vue
@@ -5,7 +5,7 @@
         <div class="mt-5 container-fluid">
             <nav class="nav nav-pills nav-justified nav-tabs">
                 <router-link class="nav-item nav-link fw-bold" :to="{ name: 'ManagerAddTutorielCountry' }">Pays</router-link>
-                <router-link class="nav-item nav-link fw-bold" disable :to="{ name: 'ManagerAddTutorielSteps' }">Procedure génerale(Nom pays)</router-link>
+                <router-link class="nav-item nav-link fw-bold" disable :to="{ name: 'ManagerAddTutorielSteps' }">Procedure génerale({{ country ? country?.short_name : 'Nom pays' }})</router-link>
                 <a class="nav-item nav-link" href="#">Université</a>
             </nav>
 
@@ -14,7 +14,14 @@
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
+    computed: {
+        ...mapGetters('countryStep', {
+            country: 'getCountrySelected'
+        }),
+ 
+    },
     name: "AddTutorial",
     mounted() {
         //redirect to the first form
--- src/views/ManagerViews/Tutorials/AddTutoriel/CountryForm.vue
+++ src/views/ManagerViews/Tutorials/AddTutoriel/CountryForm.vue
@@ -1,27 +1,76 @@
 <template>
     <div>
-        <form>
+        <error-modal-component v-if="unexpectedError"></error-modal-component>
+        <form novalidate v-on:submit.prevent="onsubmit">
             <div class="form-group row">
-                <label for="conutry" class="col-sm-2 col-form-label fw-bold">Selectionner le pays</label>
+                <label for="country" class="col-sm-2 col-form-label fw-bold">Selectionner le pays*</label>
                 <div class="col-sm-4 mb-3">
-                  <select class="form-select" id="conutry">
-                    <option value="">TEST 1</option>
-                    <option value="">TEST 145</option>
-                  </select>
+                    <select class="form-select" id="country" :class="[v$.country.$error ? 'is-invalid' : '']" v-model="state.country">
+                        <option selected value="" disabled>Selectionner un pays</option>
+                        <option v-for="country in listOfCoutries" :key="country.id" :value="country">{{ country.name }}</option>
+                    </select>
+                    <div class="invalid-feedback" v-if="v$.country.$error">
+                        <span v-for="(error, index) of v$.country.$errors" :key="index">
+                            {{ error.$message }}
+                        </span>
+                    </div>
                 </div>
-                <div  class="col-sm-4">
-                    <SubmitBtnComponent class="btn btn-success">Continuer</SubmitBtnComponent>
+                <div class="col-sm-4">
+                    <SubmitBtnComponent class="btn btn-success" @click="submit">Continuer</SubmitBtnComponent>
                 </div>
-              
+
             </div>
         </form>
     </div>
 </template>
 <script>
 import SubmitBtnComponent from '@/components/shared/SubmitBtnComponent.vue';
+import CountryStepsService from '@/Services/Manager/CountryStepsService';
+import ErrorModalComponent from '@/components/modal/ErrorModalComponent.vue';
+import { computed, reactive } from 'vue';
+import customeMessage from '@/Utils/validationMessages';
+import useVuelidate from '@vuelidate/core';
 
 export default {
+    methods: {
+        submit() {
+            this.v$.$validate();
+            this.$store.commit('countryStep/setSelectedCoutry',this.state) 
+        }
+    },
     name: "CountryFormTuto",
-    components: { SubmitBtnComponent }
+    components: { SubmitBtnComponent },
+
+
+    data() {
+        return {
+            listOfCoutries: '',
+            unexpectedError: false,
+        }
+    },
+
+    setup() {
+        const state = reactive({
+            country: ''
+        })
+
+        const rules = computed(() => {
+            return {
+                country: { required: customeMessage('country', 'required') }
+            }
+        })
+
+        const v$ = useVuelidate(rules, state);
+        return { state, v$ }
+    },
+
+    created() {
+        CountryStepsService.getListCountries().then((countries) => {
+            this.listOfCoutries = countries.data;
+        }).catch(() => {
+            this.unexpectedError = true;
+        });
+    },
+    components: { SubmitBtnComponent, ErrorModalComponent },
 }
-</script>
+</script>  
