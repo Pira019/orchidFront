4445749bd2197aad6b31fcc5513f69fc1b4c450a
pires lufungula <pires00243@gmail.com>
2023-07-05 16:11:21 -0400

    Add coutries list page

---
+++ src/views/ManagerViews/Tutorials/Countries/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Spinner v-if="loading"></Spinner>
+    <CardComponent :data="steps">
+      Nombre d'étapes
+    </CardComponent>
+  </div>
+</template>
+<script>
+import CardComponent from '@/components/shared/CardComponent.vue';
+import Spinner from '@/components/shared/Spinner.vue'
+//list of countries 
+export default {
+  data() {
+    return {
+      steps: [],
+      loading: false
+    }
+  },
+  created() {
+    this.loading = true;
+    this.$store.dispatch('countryStep/getAllCountry').then((response) => {
+      this.steps = response.data
+      this.loading = false;
+    })
+  },
+  components: { CardComponent, Spinner },
+  name: "CountriesHome"
+}
+</script>
--- src/views/ManagerViews/Tutorials/Index.vue
+++ src/views/ManagerViews/Tutorials/Index.vue
@@ -1,25 +1,9 @@
 <template>
     <div class="container-fluid">
-        <header-tuto></header-tuto>
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="..." alt="Drapeau pays">
-            <div class="card-body">
-                <h5 class="card-title">Nom pays</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Nombre d'étudiants</li>
-                <li class="list-group-item">Date de publication :</li>
-                <li class="list-group-item">Date Création :</li>
-                <li class="list-group-item">Dérniere modification :</li>
-            </ul>
-            <div class="card-body">
-                <a href="#" class="btn btn-success">Go somewhere</a>
-            </div>
-        </div>
-        
-    </div>
+      <h1>Header</h1> 
+      <router-view class="mt-5"></router-view> 
+    </div> 
+ 
 </template>
 
 <script>
