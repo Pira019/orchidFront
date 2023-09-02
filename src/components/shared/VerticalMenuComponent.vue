68bdf091192bd41133adf6d8bdadfb00b76975cb
pires lufungula <pires00243@gmail.com>
2023-09-02 00:35:29 -0400

    Add vertical menu component

---
+++ src/components/shared/VerticalMenuComponent.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">  
+        <h1 class="text-muted h2">Etape</h1>             
+        <a class="nav-link text-black fw-bold mr-3" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home"
+            aria-selected="true">Home</a>        
+    </div>
+</template>
+
+<script>
+export default {
+    props : {
+        title:{}
+    }
+}
+</script>
