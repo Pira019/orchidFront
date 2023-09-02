4dd5a5634b519e2cf9ab12fba344ff06b8d928fd
pires lufungula <pires00243@gmail.com>
2023-09-01 20:41:17 -0400

    Adapte Card component

--- src/components/shared/CardComponent.vue
+++ src/components/shared/CardComponent.vue
@@ -2,13 +2,12 @@
      <div class="container-fluid"> 
         <div class="row justify-content-evenly" v-if="list.lenght !== 0">
             <div class="card col-md-4 mb-3" v-for="(list, index) in list" :key="index" style="width: 18rem;">
-                <img class="card-img-top" :src="list?.flag_url" alt="Drapeau pays">
+              <img class="card-img-top" :src="list?.flag_url" alt="Drapeau pays">
                 <div class="card-body">
-                    <h5 class="card-title">{{ list?.name }}</h5>
-                    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>-->
-                </div>
-                <ul class="list-group list-group-flush">
+                    <h5 class="card-title" v-if="!onlyHeader">{{ list?.name }}</h5>
+                    <router-link  v-if="onlyHeader" :to="{ name: routeName, params:{id: list?.id  } }" class="text-decoration-none text-black fw-bolder">{{ list?.name }}</router-link>  
+                </div> 
+                <ul class="list-group list-group-flush" v-if="!onlyHeader" >
 
                     <li class="list-group-item">
                         <slot>Nombre étape : </slot> <span class="badge bg-dark">  {{ list?.number_of_steps }} </span>
@@ -17,15 +16,14 @@
                 <li class="list-group-item">Date Création :</li>
                 <li class="list-group-item">Dérniere modification :</li>-->
                 </ul>
-                <div class="card-body">
+                <div class="card-body"  v-if="!onlyHeader">
                     <router-link :to="{ name: 'ManagerCountrySteps', params:{id: list?.id  } }" class="btn btn-success">Voir les étapes</router-link>              
-                </div>
-            </div>
-            
+                </div>  
+            </div>            
         </div>
 
         <div class="row justify-content-evenly" v-else>
-             <p>jfjbfj</p>
+             <p>Aucune entrée !!!</p>
         </div>
     </div>
 </template>
@@ -33,6 +31,13 @@
 <script>
 export default {
   
-    props:["list"] 
+    props:{
+        list : {},
+        routeName : '#',
+        onlyHeader : {
+            type : Boolean,
+            default : false
+        }
+    }
 }
 </script>
