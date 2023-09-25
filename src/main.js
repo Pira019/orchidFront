c6b110ddac69569e19eae570dad8bb6abc1a0ab0
pires lufungula <pires00243@gmail.com>
2023-09-24 23:46:24 -0400

    Add faSchool icon

--- src/main.js
+++ src/main.js
@@ -18,10 +18,10 @@ import App from './App.vue'
 import './styles.scss'
 
 /* import specific icons */  
-import {faBars, faBullseye, faCircleCheck, faEnvelope, faEye, faEyeSlash, faGlobe, faHandshake, faHeartCircleCheck, faList, faPen, faPersonChalkboard, faPlus, faSpinner, faTrash, faTriangleExclamation,faUsers } from '@fortawesome/free-solid-svg-icons'
+import {faBars, faBullseye, faCircleCheck, faEnvelope, faEye, faEyeSlash, faGlobe, faHandshake, faHeartCircleCheck, faList, faPen, faPersonChalkboard, faPlus, faSchool, faSpinner, faTrash, faTriangleExclamation,faUsers } from '@fortawesome/free-solid-svg-icons'
 import { faInstagramSquare, faMailchimp, faWhatsappSquare} from '@fortawesome/free-brands-svg-icons'
 import { faFolderOpen } from '@fortawesome/free-regular-svg-icons'
 
  
-library.add(faSpinner,faList,faGlobe,faWhatsappSquare,faEnvelope,faInstagramSquare,faPlus,faMailchimp,faHandshake,faBullseye,faHeartCircleCheck,faCircleCheck,faEye,faEyeSlash,faTriangleExclamation,faBars,faFolderOpen,faPersonChalkboard,faTrash,faPen,faUsers) 
+library.add(faSpinner,faSchool,faList,faGlobe,faWhatsappSquare,faEnvelope,faInstagramSquare,faPlus,faMailchimp,faHandshake,faBullseye,faHeartCircleCheck,faCircleCheck,faEye,faEyeSlash,faTriangleExclamation,faBars,faFolderOpen,faPersonChalkboard,faTrash,faPen,faUsers) 
 createApp(App).use(router).use(store).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
