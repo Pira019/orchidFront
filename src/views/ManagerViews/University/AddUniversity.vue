ce95a216f4a000bae16b09eac4baef2f3e852463
pires lufungula <pires00243@gmail.com>
2023-10-01 03:14:13 -0400

    Add

--- src/views/ManagerViews/University/AddUniversity.vue
+++ src/views/ManagerViews/University/AddUniversity.vue
@@ -1,63 +1,74 @@
 <template>
     <div>
-        <h2 class="m-5 text-success">Ajouter une université</h2>
-        <form class="mx-5 my-5" novalidate v-on:submit.prevent="submit">
-            <div class="row mb-3" id="place">
-                <div class="col">
-                    <label for="pays">Pays*</label>
-                    <select class="form-select" id="pays">
-                        <option selected>Choisir un pays...</option>
-                        <option v-for="country in countries" :key="country.id" :value="country.id">{{ country.name }}</option>
-                    </select>
-                </div>
-                <div class="col" v-if="isOtherCity">
-                    <label for="VilleAutre">Ville*</label>
-                    <input type="text" id="VilleAutre" class="form-control" placeholder="Last name">
+            <h2 class="m-5 text-success">Ajouter une université</h2>
+            <form class="mx-5 my-5" novalidate v-on:submit.prevent="submit">
+                <div class="row mb-3" id="place">
+                    <div class="col">
+                        <label for="pays" :class="[v$.country_id.$error ? 'text-danger' : '']">Pays*</label>
+                        <select class="form-select" id="pays" v-model.number="state.country_id" @change="loadCities" :class="[v$.country_id.$error ? 'is-invalid' : '']">
+                            <option selected value="" disabled>Choisir un pays...</option>
+                            <option v-for="country in countries" :key="country.id" :value="country.id">{{ country.name }}</option>
+                        </select>
+                        <div class="invalid-feedback" v-if="v$.country_id.$error">
+                                    <span v-for="(error, index) of v$.country_id.$errors" :key="index">
+                                        {{ error.$message }}
+                                    </span>
+                         </div>
+                    </div>
+                    <div class="col" v-if="isOtherCity">
+                        <label for="VilleAutre" :class="[v$.city_name.$error ? 'text-danger' : '']">Ville*</label>
+                        <input type="text" id="VilleAutre" class="form-control" placeholder="Ex: Paris, Rabat , Montréal" v-model="city" name="VilleAutre"  :class="[v$.city_name.$error ? 'is-invalid' : '']">
+                        <div class="invalid-feedback" v-if="v$.city_name.$error">
+                                    <span v-for="(error, index) of v$.city_name.$errors" :key="index">
+                                        {{ error.$message }}
+                                    </span>
+                         </div>
+                    </div>
+
+                    <div class="col" v-if="!isOtherCity">
+                        <label for="Ville" :class="[v$.city_name.$error ? 'text-danger' : '']">Ville*</label>
+                        <select class="form-select" id="Ville" :class="[v$.city_name.$error ? 'is-invalid' : '']" v-model="state.city_name" @change="showCityInput">
+                            <option selected value="">Choisir ville ...</option>
+                            <option v-for="city in cities" :key="city.id" :value="city.name">{{ city.name }}</option>
+                            <option value="other_">Autre</option>
+                        </select>
+                        <div class="invalid-feedback" v-if="v$.city_name.$error">
+                                    <span v-for="(error, index) of v$.city_name.$errors" :key="index">
+                                        {{ error.$message }}
+                                    </span>
+                         </div>
+                    </div>
                 </div>
 
-                <div class="col" v-if="!isOtherCity">
-                    <label for="inputEmail4">Ville*</label>
-                    <select class="form-select" id="inputGroupSelect04">
-                        <option selected>Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option>Autre</option>
-                    </select>
-                </div>
-            </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="university">Nom université*</label>
+                        <input type="text" class="form-control" id="university"  placeholder="Ex : Université de montréal, Université de kinshasa ...">
+                    </div>
+                    <div class="col">
+                        <label for="sigle">En sigle</label>
+                        <input type="text" class="form-control" id="sigle" placeholder="Ex : UQO, UNIKIN">
+                    </div>
 
-            <div class="row mb-3">
-                <div class="col">
-                    <label for="inputEmail4">Nom université*</label>
-                    <input type="text" class="form-control"
-                        placeholder="Ex : Université de montréal, Université de kinshasa ...">
-                </div>
-                <div class="col">
-                    <label for="sigle">En sigle</label>
-                    <input type="text" class="form-control" id="sigle" placeholder="Ex : UQO, UNIKIN">
+                    <div class="col">
+                        <label for="url">Site web*</label>
+                        <input type="url" class="form-control" id="url" placeholder="Ex : UQO, UNIKIN">
+                    </div>
                 </div>
 
-                <div class="col">
-                    <label for="url">Site web*</label>
-                    <input type="url" class="form-control" id="url" placeholder="Ex : UQO, UNIKIN">
-                </div>
-            </div>
-
-            <div class="d-flex justify-content-between">
+                <div class="d-flex justify-content-between">
                  
-                <div>
-                    <submit-btn-component class="btn btn-success m-2">Enregister</submit-btn-component>
+                    <div>
+                        <submit-btn-component class="btn btn-success m-2" type="submit"  @click="save">Enregister</submit-btn-component>
+                    </div>
+
+                    <div>
+                        <button class="btn btn-warning m-2" type="reset">Effacer</button>
+                        <button class="btn btn-outline-danger m-2" type="reset">Annuler</button>
+                    </div> 
                 </div>
-
-                <div>
-                    <button class="btn btn-warning m-2" type="reset">Effacer</button>
-                    <button class="btn btn-outline-danger m-2" type="reset">Annuler</button>
-                </div> 
-            </div>
-
-
-        </form>
-    </div>
+            </form>
+        </div>
 </template>
 
 <script>
@@ -67,12 +78,48 @@ import customeMessage from '@/Utils/validationMessages'
 import useVuelidate from '@vuelidate/core'
 import { mapGetters } from 'vuex'
 export default {
+  methods: {
+    save(){
+
+        if(this.isOtherCity){
+            this.state.city_name = this.city;
+        }
+
+        this.v$.$validate();
+        if(this.v$.$error){
+            return;
+        }
+
+        console.log(this.state)
+    },
+
+    loadCities(){
+       this.isOtherCity = false; 
+       this.state.city_name = ''; 
+       this.city = ''
+       if(!this.state.country_id){
+        return;
+       }
+       this.$store.dispatch('country/getCities',this.state.country_id).then((response) => {
+         this.cities = response.data;
+       });
+    },
+
+    showCityInput() {
+            if (this.state.city_name === 'other_') {
+                this.isOtherCity = true;
+            }
+
+        }
+    },
     created() {
         this.$store.dispatch('country/getCoutries');
     },
     data() {
         return {
             isOtherCity : false,
+            city : '',
+            cities : []
         }
     },
 
@@ -87,14 +134,14 @@ export default {
             name: '',
             shortName: '',
             city_name: '',
-            country_id: 0,
+            country_id: '',
             webSite: '',
         })
 
         const rules = computed(() => {
             return {
-                title: { required: customeMessage('title', 'required') },
-                order: { required: customeMessage('order', 'required') }
+                country_id: { required: customeMessage('country', 'required') }, 
+                city_name: { required: customeMessage('city', 'required') }, 
             }
         })
 
