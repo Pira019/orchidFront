7616d13bcf8b8fbb1d2226ff792099fc7cb597a9
pires lufungula <pires00243@gmail.com>
2023-10-01 13:12:13 -0400

    Add UniversityService manager service & save feature

---
+++ src/Services/Manager/UniversityService.js
@@ -0,0 +1,27 @@
+
+import axios, * as others from 'axios';
+export default class UniversityService{
+    static baseApiUrl = import.meta.env.VITE_APP_API_URL + "manager/university/";
+    static storedToken = localStorage.getItem('authUserToken'); //  
+    /**
+   * A class for interacting with the user service API.
+   *
+   * @param {string} baseApiUrl - The base URL of back end API.
+   */
+
+    constructor() { }      
+    
+
+    static axiosInstance = axios.create({
+        baseURL: this.baseApiUrl,
+        headers: {
+            'Authorization': `Bearer ${UniversityService.storedToken}`
+        }
+    });
+ 
+    static async save(newUniversity){
+        const endPoind = ''
+        return this.axiosInstance.post(endPoind,newUniversity);  
+    }  
+ 
+}
--- src/views/ManagerViews/University/AddUniversity.vue
+++ src/views/ManagerViews/University/AddUniversity.vue
@@ -2,11 +2,14 @@
     <div>
             <h2 class="m-5 text-success">Ajouter une université</h2>
             <form class="mx-5 my-5" novalidate v-on:submit.prevent="submit">
+                <success-alert :show="isSucced" :response="msg">
+                    <p> La sauvegarde de l'université a réussi !</p>
+                </success-alert>
                 <div class="row mb-3" id="place">
-                    <div class="col">
+                    <div class="col-12 col-md">
                         <label for="pays" :class="[v$.country_id.$error ? 'text-danger' : '']">Pays*</label>
                         <select class="form-select" id="pays" v-model.number="state.country_id" @change="loadCities" :class="[v$.country_id.$error ? 'is-invalid' : '']">
-                            <option selected value="" disabled>Choisir un pays...</option>
+                            <option selected value="">Choisir un pays...</option>
                             <option v-for="country in countries" :key="country.id" :value="country.id">{{ country.name }}</option>
                         </select>
                         <div class="invalid-feedback" v-if="v$.country_id.$error">
@@ -15,7 +18,7 @@
                                     </span>
                          </div>
                     </div>
-                    <div class="col" v-if="isOtherCity">
+                    <div class="col-12 col-md" v-if="isOtherCity">
                         <label for="VilleAutre" :class="[v$.city_name.$error ? 'text-danger' : '']">Ville*</label>
                         <input type="text" id="VilleAutre" class="form-control" placeholder="Ex: Paris, Rabat , Montréal" v-model="city" name="VilleAutre"  :class="[v$.city_name.$error ? 'is-invalid' : '']">
                         <div class="invalid-feedback" v-if="v$.city_name.$error">
@@ -41,30 +44,40 @@
                 </div>
 
                 <div class="row mb-3">
-                    <div class="col">
-                        <label for="university">Nom université*</label>
-                        <input type="text" class="form-control" id="university"  placeholder="Ex : Université de montréal, Université de kinshasa ...">
+                    <div class="col col-12 col-md">
+                        <label for="university" :class="[v$.name.$error ? 'text-danger' : '']">Nom université*</label>
+                        <input type="text" class="form-control" id="university" v-model.trim="state.name"  :class="[v$.name.$error ? 'is-invalid' : '']" placeholder="Ex : Université de montréal, Université de kinshasa ...">
+                        <div class="invalid-feedback" v-if="v$.name.$error">
+                                    <span v-for="(error, index) of v$.name.$errors" :key="index">
+                                        {{ error.$message }}
+                                    </span>
+                         </div>
                     </div>
-                    <div class="col">
+                    <div class="col-12 col-md">
                         <label for="sigle">En sigle</label>
-                        <input type="text" class="form-control" id="sigle" placeholder="Ex : UQO, UNIKIN">
+                        <input type="text" class="form-control" style="text-transform:uppercase;"  id="sigle" placeholder="Ex : UQO, UNIKIN"  v-model.trim="state.shortName">
                     </div>
 
-                    <div class="col">
-                        <label for="url">Site web*</label>
-                        <input type="url" class="form-control" id="url" placeholder="Ex : UQO, UNIKIN">
+                    <div class="col-12 col-md">
+                        <label for="url" :class="[v$.webSite.$error ? 'text-danger' : '']">Site web*</label>
+                        <input type="url" class="form-control" id="url"   :class="[v$.webSite.$error ? 'is-invalid' : '']" v-model.trim="state.webSite" placeholder="Ex : https://www.orchid-campus.com/">
+                        <div class="invalid-feedback" v-if="v$.webSite.$error">
+                                    <span v-for="(error, index) of v$.webSite.$errors" :key="index">
+                                        {{ error.$message }}
+                                    </span>
+                         </div>
                     </div>
                 </div>
 
                 <div class="d-flex justify-content-between">
                  
                     <div>
-                        <submit-btn-component class="btn btn-success m-2" type="submit"  @click="save">Enregister</submit-btn-component>
+                        <submit-btn-component class="btn btn-success m-2" type="submit" :loading="isLoading"  @click="save">Enregister</submit-btn-component>
                     </div>
 
                     <div>
-                        <button class="btn btn-warning m-2" type="reset">Effacer</button>
-                        <button class="btn btn-outline-danger m-2" type="reset">Annuler</button>
+                        <button class="btn btn-warning m-2" @click="resetState">Effacer</button>
+                        <button class="btn btn-outline-danger m-2" >Annuler</button>
                     </div> 
                 </div>
             </form>
@@ -76,7 +89,8 @@ import SubmitBtnComponent from '@/components/shared/SubmitBtnComponent.vue'
 import { computed, reactive } from 'vue'
 import customeMessage from '@/Utils/validationMessages'
 import useVuelidate from '@vuelidate/core'
-import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex' 
+import SuccessAlert from '@/components/shared/Alert/SuccessAlert.vue'
 export default {
   methods: {
     save(){
@@ -90,8 +104,27 @@ export default {
             return;
         }
 
-        console.log(this.state)
+        this.isLoading = true;
+        this.$store.dispatch('universityManager/saveUniversity',this.state).then((response) => {
+            this.isSucced = true;
+            this.v$.$reset();
+            this.resetState();
+            this.isLoading = false;
+            this.msg = response.data.status 
+        }).catch((error)=> {
+
+        });
+
+        this.isSucced = false;
+
     },
+
+    resetState() {
+            for (let key in this.state) {
+                this.state[key] = '';
+            }
+            this.city = '';
+        },
 
     loadCities(){
        this.isOtherCity = false; 
@@ -119,7 +152,10 @@ export default {
         return {
             isOtherCity : false,
             city : '',
-            cities : []
+            cities : [],
+            isSucced : false,
+            isLoading : false,
+            msg:''
         }
     },
 
@@ -137,19 +173,21 @@ export default {
             country_id: '',
             webSite: '',
         })
-
         const rules = computed(() => {
             return {
-                country_id: { required: customeMessage('country', 'required') }, 
-                city_name: { required: customeMessage('city', 'required') }, 
+                country_id: { required: customeMessage('country', 'required') },
+                city_name: { required: customeMessage('city', 'required') },
+                name: { required: customeMessage('name_universiry', 'required') },
+                webSite: {  required:customeMessage('webSite', 'required'), url  :customeMessage('', 'url') },
             }
         })
 
+        
         const v$ = useVuelidate(rules, state);
         return { state, v$ }
     },
 
-    components: { SubmitBtnComponent },
+    components: { SubmitBtnComponent, SuccessAlert },
 
 }
 </script> 
