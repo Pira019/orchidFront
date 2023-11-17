dfd601fd770009d852dbee91a545cac941885b97
pires lufungula <pires00243@gmail.com>
2023-11-16 20:26:00 -0500

    Update program model

--- src/model/programs.js
+++ src/model/programs.js
@@ -9,10 +9,12 @@
 
 export const programModel = {
 
+    title: '',
     cycle: 0,
     duration: 0,
     nbrCredit: 0,
     languages: '',
     admission_scheme: '',
+    disciplinary_sector: '', 
 
 }
