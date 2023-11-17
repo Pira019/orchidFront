a84a0f30e007b59b3f3962e457f15265d6d48f26
pires lufungula <pires00243@gmail.com>
2023-11-16 23:21:54 -0500

    Edit Static modal and make it extendable

---
+++ src/components/modal/StaticbackdropModal.vue
@@ -0,0 +1,63 @@
+<template>
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+               
+                    <div class="modal-header">
+                        <h5 class="modal-title attention-title" id="staticBackdropLabel">{{  title }}</h5>
+                        <button type="button" class="btn-close" @click="close(), $emit('isConfirm', false)"
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <slot>
+                        <p>Vous êtes sur le point de supprimer une étape</p>
+                        <p class="confirmation-message"> Cette action est irréversible. Êtes-vous sûr de vouloir continuer ?
+                        </p> 
+                        </slot>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary"
+                            @click="close(), $emit('isConfirm', false)">Annuler</button>
+                        <button type="button" class="btn btn-danger" @click="$emit('isConfirm', true)">Supprimer</button>
+                    </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        closeModal:{},
+        title:{
+            default: 'Confirmation de Suppression'
+        } 
+  },
+    watch: {
+        closeModal(newValue) {
+            if (newValue) {
+                this.close()
+            }
+            else {
+                this.showModal()
+            }
+
+        }
+    },
+    methods: {
+        close() {
+            $(document).ready(function () {
+                $("#staticBackdrop").modal('hide');
+            });
+        },
+
+        showModal() {
+            $(document).ready(function () {
+                $("#staticBackdrop").modal('show');
+            });
+        }
+    },
+}
+</script>
