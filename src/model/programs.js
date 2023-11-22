3c43593c447439d323005e9eb3b43902cd390a68
pires lufungula <pires00243@gmail.com>
2023-11-22 10:22:25 -0500

    Update programModel

--- src/model/programs.js
+++ src/model/programs.js
@@ -1,6 +1,12 @@
 /**
  * @typedef {Object} program 
+ * @property {string} title
  * @property {number} cycle
+ * @property {number} duration
+ * @property {number} nbrCredit
+ * @property {string} languages
+ * @property {string} admission_scheme
+ * @property {string} disciplinary_sector
  */
 
 /**
@@ -8,13 +14,14 @@
  */
 
 export const programModel = {
-
-    title: '',
+ 
+    program_name: '',
     cycle: 0,
     duration: 0,
     nbrCredit: 0,
     languages: '',
     admission_scheme: '',
     disciplinary_sector: '', 
+    discipline_description: '', 
 
 }
