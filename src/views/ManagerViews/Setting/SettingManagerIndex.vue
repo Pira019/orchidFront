d951bceb8862bd11d235dc1a50e7e0579cb375ed
pires lufungula <pires00243@gmail.com>
2024-01-18 12:32:52 -0500

    add signature

---
+++ src/views/ManagerViews/Setting/SettingManagerIndex.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <div class="container-fluid">
+        <h1>Paramètres</h1>  
+       <router-view class="mt-5"></router-view>
+    </div>
+    
+</template>
---
+++ src/views/ManagerViews/Setting/WaterMark.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="Filigrane image">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Filigrane principale Orchid-campus </h5>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script> 
+export default{
+    
+    mounted(){
+
+    }
+}
+</script>
