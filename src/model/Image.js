eee8cb4020ca8e4a42fe6b6b77319cd6b13e8a0e
pires lufungula <pires00243@gmail.com>
2024-01-19 02:46:49 -0500

    Add image and watermark model

---
+++ src/model/Image.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} Image 
+ * @property {string} imageable_id 
+ * @property {string} url 
+ */ 
+ 
+
+/**
+ * @type {Image}
+ */
+
+export const Image = {
+ 
+    imageable_id: null, 
+    url: null, 
+
+}
