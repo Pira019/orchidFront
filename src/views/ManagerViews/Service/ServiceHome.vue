8a050ae3786b1e159757e00c524791d89ab709f2
pires lufungula <pires00243@gmail.com>
2024-03-19 08:33:19 -0400

    Add route en page services

---
+++ src/views/ManagerViews/Service/ServiceHome.vue
@@ -0,0 +1,12 @@
+<template> 
+
+        <router-link :to="{name:'ManagerServiceAdd'}" class="btn btn-success">Ajouter</router-link>  
+   
+</template>
+
+<script>
+import PageTitle from '@/components/shared/Manager/PageTitle.vue';
+export default{
+    components: { PageTitle},
+}
+</script>
