8828be8d93ec7fed5654c5110cf72de6c4b3a128
pires lufungula <pires00243@gmail.com>
2024-04-01 00:54:41 -0400

    Add service view

---
+++ src/views/ManagerViews/Service/ServiceDetail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container-fluid">
+        <h1>test</h1>
+
+        <div>
+            <table-component>
+                <template #TableHeader>
+                    <th scope="col">Disciplines</th>
+                    <th scope="col">Etapes</th> 
+                    <th scope="col">Universités</th> 
+                    <th scope="col">Action</th>
+                </template>
+            </table-component>
+        </div>
+    </div>
+</template>
+
+<script>
+import TableComponent from '@/components/shared/TableComponent.vue'
+
+export default{
+  components: { TableComponent },}
+</script>
