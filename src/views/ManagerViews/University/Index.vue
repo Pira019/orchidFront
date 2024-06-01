757e1d570f65a9f3af1c91a73f9c828b75cc5f7d
pires lufungula <pires00243@gmail.com>
2024-06-01 03:06:22 -0400

    fix and clean

--- src/views/ManagerViews/University/Index.vue
+++ src/views/ManagerViews/University/Index.vue
@@ -1,72 +1,80 @@
 <template>
     <div class="container-fluid">
-        <error-modal-component v-if="unexpectedError"></error-modal-component>  
-            <university-layout :isShowBtn="true"> 
-                <div class="input-group" id="search">
-                    <select class="form-select" id="inputGroupSelect04" v-model.number="selectedCountry">
-                        <option selected disabled value="">Choisir un pays...</option>
-                        <option selected disabled value="" v-if="!countries.length">Aucune université dans le systeme...
-                        </option>
-                        <option v-for="country in countries" :key="country.id" :value="country.id">{{ country.name }}
-                        </option>
-                    </select>
-                    <div class="input-group-append">
-                        <button class="btn btn-success" @click="getUniversities" type="button"><font-awesome-icon
-                                icon="fa-magnifying-glass" size="1x" /></button>
-                    </div>
-                </div>
-                 
-                <!--Start list university-->
+        <form  v-on:submit.prevent="getUniversities">
+        <div class="input-group" id="search">
+            <select required class="form-select" id="inputGroupSelect04" v-model.number="selectedCountry">
+                <option selected disabled value="">Selectionnez un pays...</option>
+                <option disabled value="" v-if="!countries.length">Aucune université dans le systeme...
+                </option>
+                <option v-for="country in countries" :key="country.id" :value="country.id">{{ country.name }}
+                </option>
+            </select>
+            <div class="input-group-append">
+                <button class="btn btn-success" type="submit"><font-awesome-icon
+                        icon="fa-magnifying-glass" size="1x" /></button>
+            </div>
+        </div>
+       
+        </form>
+        <!--Start list university-->
 
-                <div class="mt-5"> 
-                   <Spinner v-if="isLoading"/>
-                   <card-component v-if="!isLoading" :routeName="'ManagerUniversiteDetail'" :list="univestities" :onlyHeader="true" :isUniversityList="true">
-                        <span>Ville : </span>
-                    </card-component>
-                </div> 
-                <!--End list university--> 
-            
-        </university-layout>
+        <div class="mt-4">
+            <Spinner v-if="isLoading" />
+            <request-alert :isSucceed="false" :responseMessage="requestMessageError" v-show="!isLoading && requestMessageError"></request-alert>
+            <card-component v-if="!isLoading && !requestMessageError" :routeName="'ManagerUniversiteDetail'" :list="univestities"
+                :onlyHeader="true" :isUniversityList="true">
+                <span>Ville : </span>
+            </card-component>
+        </div>
+        <!--End list university-->
     </div>
 </template>
 <script>
-import ErrorModalComponent from '@/components/modal/ErrorModalComponent.vue';
-import { navigateToRoute } from '@/Utils/Navigation'; 
-import UniversityLayout from './UniversityLayout.vue';
+import ErrorModalComponent from '@/components/modal/ErrorModalComponent.vue'; 
 import Spinner from '@/components/shared/Spinner.vue';
 import CardComponent from '@/components/shared/CardComponent.vue';
+import errorMessage from '@/Utils/ErrorMessage';
+import RequestAlert from '@/components/shared/Alert/RequestAlert.vue';
 export default {
-    components: { ErrorModalComponent, UniversityLayout,Spinner, CardComponent },
+    components: { ErrorModalComponent, Spinner, CardComponent, RequestAlert },
     data() {
         return {
             countries: [],
-            selectedCountry: '',
-            unexpectedError: false,
+            selectedCountry: '', 
             univestities: [],
-            isLoading : false,
+            isLoading: false,
+            requestMessageError: null
         }
     },
     methods: {
-        
-        getUniversities() {
+
+        getUniversities() 
+        {
             this.isLoading = true;
+            this.requestMessageError = null;
             this.$store.dispatch('universityManager/universitiesByCountryId', this.selectedCountry).then((response) => {
                 this.univestities = response.data;
                 this.isLoading = false;
-            }).catch((error) => {
-                navigateToRoute.call(this, error.response.status, 'manager403');
-                this.unexpectedError = false;
+            }).catch((error) =>
+            {
+               this.requestMessageError = errorMessage(error);
+            }).finally(()=>
+            {                
                 this.isLoading = false;
-            }); 
+            });
         }
     },
-    mounted() {
-        this.$store.dispatch('countryManager/countriesWithUniversities').then((response) => {
-            this.countries = response.data;
-        }).catch((error) => {
-            navigateToRoute.call(this, error.response.status, 'manager403');
-            this.unexpectedError = false;
-        });
+    mounted() 
+    {
+        this.isLoading = true;
+        this.$store.dispatch('countryManager/countriesWithUniversities')
+            .then((response) => {
+                this.countries = response.data;
+            }).catch((error) => {
+                this.requestMessageError = errorMessage(error, true); 
+            }).finally(()=>{
+                this.isLoading = false;
+            });
     },
 
 }
--- src/views/ManagerViews/University/UniversityLayout.vue
+++ src/views/ManagerViews/University/UniversityLayout.vue
@@ -1,35 +1,18 @@
 <template>
-  <section>
-    <header class="mb-3">
-    <div class="mb-5 justify-content-between d-flex">
-      <h1 class="text-success">{{ title }}</h1>
-      <div v-if="isShowBtn">
-        <router-link
-          :to="{ name: 'ManagerUniversiteAdd' }"
-          class="btn btn-success"
-        >
-          <font-awesome-icon icon="fa-plus" size="1x" />Ajouter
-        </router-link>
-      </div>
-    </div>
-  </header>
-    
-  <body class="p-3 border rounded">
-    <slot></slot>
-  </body>
-  </section>
- 
+  <div>
+  <div>
+    <page-title>
+      Universités
+    </page-title>
+  </div>
+
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
+import PageTitle from '@/components/shared/Manager/PageTitle.vue';
 export default {
-  props: {
-    title: {
-      default: "Liste des Universités",
-    },
-    isShowBtn: {
-      default: false,
-    },
-  },
+  components: { PageTitle }, 
 };
 </script>
