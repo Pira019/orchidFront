112e51fe5396df1b05332c24008f9b096acf9aed
pires lufungula <pires00243@gmail.com>
2024-06-03 19:50:10 -0400

    clean code

--- src/views/ManagerViews/University/UniversityLayout.vue
+++ src/views/ManagerViews/University/UniversityLayout.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
-  <div>
-    <page-title>
-      Universités
-    </page-title>
-  </div>
-
-  <router-view></router-view>
-</div>
+    <div>
+      <page-title :isLoading="isDataLoading" :requestResponseMessage="requestResponse">
+        {{ pageTitle || 'Universités' }}
+      </page-title>
+    </div>
+    <router-view v-show="!isDataLoading && !requestResponse" :requestResponse="requestResponse"></router-view>
+  </div> 
 </template>
 
 <script>
 import PageTitle from '@/components/shared/Manager/PageTitle.vue';
+import { mapGetters } from 'vuex';
 export default {
   components: { PageTitle }, 
+  computed: {
+        ...mapGetters('ManagerStore', {
+            isDataLoading: 'isDataLoading',
+            requestResponse: 'requestResponse', 
+            pageTitle: 'getPageTitle', 
+        })
+    },
+  
 };
 </script>
